6dcab0d60fccd930e2a0d7e721b1d8116a5e3a6b
Brandon Unzueta <unzueta.brandon@gmail.com>
2022-05-19 17:57:31 -0700

    adding a basic slider to homepage

--- shelton-site/components/Header.js
+++ shelton-site/components/Header.js
@@ -14,28 +14,28 @@ export default function Header() {
       <div className="text-xl my-auto ml-auto">
         <nav>
           <ul className="flex">
-            <li className="pr-4 opacity-60 ease-in duration-200 hover:opacity-100 ">
+            <li className="pr-4 opacity-60 ease-in duration-200 hover:opacity-100">
               <Link href="/">
                 <a>
                   <p>Home</p>
                 </a>
               </Link>
             </li>
-            <li className="pr-4 opacity-60 ease-in duration-200 hover:opacity-100 ">
+            <li className="pr-4 opacity-60 ease-in duration-200 hover:opacity-100">
               <Link href="/photos">
                 <a>
                   <p>Photos</p>
                 </a>
               </Link>
             </li>
-            <li className="pr-4 opacity-60 ease-in duration-200 hover:opacity-100 ">
+            <li className="pr-4 opacity-60 ease-in duration-200 hover:opacity-100">
               <Link href="/videos">
                 <a>
                   <p>Videos</p>
                 </a>
               </Link>
             </li>
-            <li className=" opacity-60 ease-in duration-200 hover:opacity-100 ">
+            <li className="opacity-60 ease-in duration-200 hover:opacity-100">
               <Link href="/about">
                 <a>
                   <p>About</p>
--- shelton-site/pages/index.js
+++ shelton-site/pages/index.js
@@ -1,10 +1,16 @@
 import Layout from "../components/Layout"
+import { Slide } from 'react-slideshow-image'
+import 'react-slideshow-image/dist/styles.css'
 
 export default function Home() {
   return (
     <Layout>
-      <div>
-         
+      <div id="jumbotron" className="flex w-full h-[50em] slideshow ">
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
       </div>
     </Layout>
   )
